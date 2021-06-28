a095496dda67f730ca0f978436e78a847458de6a
Weliton <weliton.filho@pathfind.com.br>
2021-06-28 18:45:00 -0300

    create project

---
+++ src/main/java/com/weatherObserver/WeatherObserverApplication.java
@@ -0,0 +1,13 @@
+package com.weatherObserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherObserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeatherObserverApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/weatherObserver/WeatherObserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.weatherObserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherObserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
