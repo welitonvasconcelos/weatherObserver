cf67ec5fdc95db69cbcebc01cd96d5e35dd86b4d
Weliton <weliton.filho@pathfind.com.br>
2021-07-11 15:44:15 -0300

    bugfix- Deserialize date format

--- src/main/java/com/weatherObserver/model/dto/WeatherConditionDTO.java
+++ src/main/java/com/weatherObserver/model/dto/WeatherConditionDTO.java
@@ -2,8 +2,14 @@ package com.weatherObserver.model.dto;
 
 import java.time.LocalDateTime;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonFormat.Shape;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
 
 import lombok.Data;
 
@@ -12,6 +18,9 @@ import lombok.Data;
 public class WeatherConditionDTO {
 	
 	@JsonProperty("LocalObservationDateTime")
+	@JsonFormat(shape = Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss[xxx]")
+	@JsonSerialize(using = LocalDateTimeSerializer.class)
+	@JsonDeserialize(using = LocalDateTimeDeserializer.class)
 	private LocalDateTime localObservationDateTime;
 	@JsonProperty("IsDayTime")
 	private Boolean isDayTime;
